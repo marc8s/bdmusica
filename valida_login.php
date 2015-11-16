87a0cac5c9711d958d7f3d1a3595a8fd292da2f8
marc8s <marcus.ecomp@gmail.com>
2015-11-15 22:04:17 -0300

    Parte 13 - Validação do usuário na base de dados

--- valida_login.php
+++ valida_login.php
@@ -5,6 +5,15 @@ $usuariot = $_POST['email'];
 $senhat = $_POST['senha'];
 echo $usuariot.' - '.$senhat;
 
+$result = $conexao->query("SELECT * FROM adminmusica WHERE email = '$usuariot' and senha = '$senhat'"); 
+$linhas = $result->num_rows;
+
+if($linhas == 0){
+	$url = 'mensagemErroBusca.html';
+	echo'<META HTTP-EQUIV=Refresh CONTENT="0; URL='.$url.'">';
+}else{
+$row = $result->fetch_object(); }
+
 /*$sql = "UPDATE musicanaescola set Escola ='$escola', Cidade ='$cidade', Identificador= '$identificador', Telefone='$telefone' WHERE Identificador = $identificador";
 $conexao->query($sql);
 $conexao->close();*/
