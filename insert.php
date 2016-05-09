28f8c2b984e2b0043b039ad96c81dadaed529b67
marc8s <marcus.ecomp@gmail.com>
2016-05-09 20:38:35 -0300

    Correção erro adição no banco de dados

--- insert.php
+++ insert.php
@@ -5,16 +5,13 @@ include_once("seguranca.php");
 if($_SESSION['email'] != ""){
 	include_once("connect.php");
 
-	$escola = $_POST['campo1'];
-	$cidade = $_POST['campo2'];
-	//$identificador = $_POST['campo3'];
-	$telefone = $_POST['campo4'];
+	$Escola = $_POST['campo1'];
+	$Cidade = $_POST['campo2'];	
+	$Telefone = $_POST['campo4'];	
+	$sql = "INSERT INTO `musicanaescola`(`Identificador`, `Escola`, `Cidade`, `Telefone`) VALUES ";	
+	$sql .= "('', '$Escola', '$Cidade','$Telefone')";
 	
-	//$sql = "INSERT INTO musicanaescola VALUES";
-	//INSERT INTO `musicanaescola`(`identificador`, `escola`, `cidade`, `telefone`) VALUES ([value-1],[value-2],[value-3],[value-4])
-	$sql = "INSERT INTO `musicanaescola`(`escola`, `cidade`, `telefone`) VALUES";
-	//$sql .= "('$escola', '$cidade', '$identificador', '$telefone')";
-	$sql .= "('$escola', '$cidade','$telefone')";
+	
 	$conexao->query($sql);
 	$conexao->close();
 	//$url = 'view.php?id='.$identificador;
